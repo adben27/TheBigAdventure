7d9df975dd0a8c34cb3cc5fdd3f1eab132dda214
leolust <leolust@outlook.fr>
2023-11-29 23:52:02 +0100

    Ajout d'un projet demo pour faire des test sur zen5

--- demo/fr/umlv/zen5/demo/Demo.java
+++ demo/fr/umlv/zen5/demo/Demo.java
@@ -1,64 +1,79 @@
 package fr.umlv.zen5.demo;
 
 import java.awt.Color;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import fr.umlv.zen5.Application;
-import fr.umlv.zen5.ApplicationContext;
 import fr.umlv.zen5.Event;
-import fr.umlv.zen5.ScreenInfo;
 import fr.umlv.zen5.Event.Action;
+import fr.umlv.zen5.KeyboardKey;
+import fr.umlv.zen5.ScreenInfo;
 
 public class Demo {
-  static class Area {
-    private Ellipse2D.Float ellipse = new Ellipse2D.Float(0, 0, 0, 0);
-    
-    void draw(ApplicationContext context, float x, float y) {
-      context.renderFrame(graphics -> {
-        // hide the previous rectangle
-        graphics.setColor(Color.ORANGE);
-        graphics.fill(ellipse);
-        
-        // show a new ellipse at the position of the pointer
-        graphics.setColor(Color.MAGENTA);
-        ellipse = new Ellipse2D.Float(x - 20, y - 20, 40, 40);
-        graphics.fill(ellipse);
-      });
-    }
-  }
+  static int x = 0;
+  static int y = 0;
   
-  public static void main(String[] args) {
-    Application.run(Color.ORANGE, context -> {
+  public static void main(String[] args) throws IOException {
+  	String tileName = "pnj/baba";
+  	BufferedImage image;
+  	try(var input = Demo.class.getResourceAsStream("/img/" + tileName + ".png")) {
+  		image = ImageIO.read(input);
+  	}
+    Application.run(Color.BLACK, context -> {
       
       // get the size of the screen
       ScreenInfo screenInfo = context.getScreenInfo();
       float width = screenInfo.getWidth();
       float height = screenInfo.getHeight();
-      System.out.println("size of the screen (" + width + " x " + height + ")");
       
-      context.renderFrame(graphics -> {
-        graphics.setColor(Color.ORANGE);
-        graphics.fill(new  Rectangle2D.Float(0, 0, width, height));
-      });
-      
-      Area area = new Area();
       for(;;) {
         Event event = context.pollOrWaitEvent(10);
         if (event == null) {  // no event
           continue;
         }
         Action action = event.getAction();
-        if (action == Action.KEY_PRESSED || action == Action.KEY_RELEASED) {
+        if (action == Action.POINTER_DOWN || action == Action.POINTER_UP) {
           System.out.println("abort abort !");
           context.exit(0);
           return;
         }
         System.out.println(event);
         
-        Point2D.Float location = event.getLocation();
-        area.draw(context, location.x, location.y);
+        if (action == Action.KEY_PRESSED) {
+          KeyboardKey key = event.getKey();
+          switch (key) {
+  					case UP: {
+  						if (y > 0) {y-=20;}
+  						break;
+  					}
+  					case DOWN: {
+  						if (y < height-20) {y+=20;}
+  						break;
+  					}
+  					case LEFT: {
+  						if (x > 0) {x-=20;}
+  						break;
+  					}
+  					case RIGHT: {
+  						if (x < width-20) {x+=20;}
+  						break;
+  					}
+  					default:
+  						break;
+  				}
+        }
+        context.renderFrame(graphics -> {
+          // hide the previous rectangle
+          graphics.setColor(Color.BLACK);
+          graphics.fill(new Rectangle2D.Float(0, 0, width, height));
+          
+          graphics.drawImage(image,x, y, null);
+        });
+
       }
     });
   }
