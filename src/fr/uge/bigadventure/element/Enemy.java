308ca40d3ce6e804ecb17e345869590e7d47e562
Adel BENNOUAR <adel.bennouar@edu.univ-eiffel.fr>
2023-12-07 13:54:47 +0100

    Ajout classe Enemy

--- src/fr/uge/bigadventure/element/Enemy.java
+++ src/fr/uge/bigadventure/element/Enemy.java
@@ -3,23 +3,26 @@ package fr.uge.bigadventure.element;
 import java.awt.Point;
 import java.util.Objects;
 
-public class Player implements Element {
+public class Enemy implements Element {
 	private final String name;
 	private final String skin;
 	public int health;
 	public final Point position;
+	private final int damage;
+	// private final String behavior;
 	
-	public Player(String name, String skin, int health, Point position) {
+	public Enemy(String name, String skin, int health, Point position, int damage) {
 		Objects.requireNonNull(name);
 		Objects.requireNonNull(skin);
 		Objects.requireNonNull(position);
-		if(health <= 0) {
+		if(health <= 0 || damage <= 0) {
 			throw new IllegalArgumentException("health or damage can't be zero or less");
-		}	
+		}
 		this.name = name;
 		this.skin = skin;
 		this.health = health;
 		this.position = position;
+		this.damage = damage;
 	}
 	
 }
